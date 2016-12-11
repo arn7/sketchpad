2117ef4b3e5e1bcef62c9a9cf585e2d923b0eb3b
Ashley <ashleynaratadam@gmail.com>
2016-12-11 04:19:17 -0600

    create first version of grid

---
+++ sketch.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+for (var i=0; i<16; i++){
+	for (var j=0; j<16; j++){
+		$("#container").append("<div class='grid'></div>"); 
+		
+	}; 
+	
+	$("#container").append("<br>"); 
+	
+	
+}
+
+
+
+}); 
+
+
