6fd83df7d9582347e8dad7f9230b66a9c7f27c0f
Ashley <ashleynaratadam@gmail.com>
2016-12-11 21:29:26 -0600

    Add first complete version of project

--- sketch.js
+++ sketch.js
@@ -1,18 +1,60 @@
+
+
 $(document).ready(function(){
 
-for (var i=0; i<16; i++){
-	for (var j=0; j<16; j++){
-		$("#container").append("<div class='grid'></div>"); 
-		
-	}; 
+	initGrid(16); 
+	sketch(); 
+	$("#reset").click(function(){
+	$("#container").empty(); 
+	var newSize = prompt("How large do you want the grid?"); 
 	
-	$("#container").append("<br>"); 
+	initGrid(newSize); 
+	var dimensions = 640/newSize; 
+	$('.grid').css({'height': dimensions + 'px', 'width': dimensions + 'px'});
+	sketch(); 
 	
 	
-}
-
-
+	
+})
 
 }); 
+
+
+
+
+function initGrid(size){
+    
+    
+    for(var i=0; i<size; i++){
+	for(var j=0; j<size; j++){
+
+      		$("#container").append("<div class='grid'></div>");
+    }; 
+  }; 
+}; 
+
+
+
+
+
+
+function sketch(){
+
+	$(".grid").on("mouseenter", function(){
+		$(this).css('background', getColor()); 
+});
+
+}; 
+
+
+function getColor(){
+	var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 
 
